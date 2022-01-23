6928c857c00ba52e31b26f92115b46fe709386d4
Tony Ware <ton8675309@gmail.com>
2022-01-23 15:18:01 -0700

    1

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -16,5 +16,17 @@ class PostsService {
     logger.log('create post res', res.data)
     AppState.posts(res.data)
   }
+
+  async removePost(id) {
+    const res = await api.delete('api/posts/' + id)
+
+    AppState.posts = AppState.posts.filter(p => p.id != post.id)
+  }
+
+  async editPost(id) {
+    const res = await api.put('api/posts/' + id)
+
+    AppState.posts = AppState.posts.splice(AppState.posts.findIndex(p => p.id == post.id))
+  }
 }
 export const postsService = new PostsService()
